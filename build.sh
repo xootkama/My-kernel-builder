04c741b171b4278805371681df4e9ab659663ecf
kuplemarkeple <86312158+kuplemarkeple@users.noreply.github.com>
2021-08-20 22:38:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ DEVICE="X00T"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=X00T_defconfig
+DEFCONFIG=wizard_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
