49c231f881b67f333eeb6532c259e5ccf4e5d567
kuplemarkeple <86312158+kuplemarkeple@users.noreply.github.com>
2021-08-20 13:08:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -189,7 +189,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="injustice"
     export KBUILD_BUILD_HOST="android-build"
-    export KBUILD_BUILD_VERSION="39"
+    export KBUILD_BUILD_VERSION="8"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
