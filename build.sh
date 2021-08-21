f8057681cb877643fa6c5cf974bddc86299360d9
kuplemarkeple <86312158+kuplemarkeple@users.noreply.github.com>
2021-08-21 16:23:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -138,7 +138,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning toolchain ||"
-		git clone --depth=1 https://github.com/Panchajanya1999/azure-clang -b master clang
+		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang -b main clang
 
 	elif [ $COMPILER = "gcc" ]
 	then
