b4c8cc8411305cc43d34348408ec0e56084d43f3
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-12 12:36:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,8 +57,8 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
 NAMA=Wizard
-VARIAN=Electro
-JENIS=HMP
+VARIAN=Overclock
+JENIS=Electro
 # Build Type
 BUILD_TYPE="Nightly"
 
@@ -377,6 +377,7 @@ gen_zip() {
 		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
 	fi
 	cd AnyKernel3 || exit
+        cp -af "$KERNEL_DIR"/init.WizardSpectrum.rc init.spectrum.rc && sed -i "s/persist.spectrum.kernel.*/persist.spectrum.kernel Tiktodz/g" init.spectrum.rc
         cp -af anykernel-real.sh anykernel.sh
 	sed -i "s/kernel.string=.*/kernel.string=$NAMA-$VARIAN/g" anykernel.sh
 	sed -i "s/kernel.for=.*/kernel.for=$JENIS/g" anykernel.sh
