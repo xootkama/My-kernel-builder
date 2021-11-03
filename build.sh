d70d33d18b7809592da5685d9020528299e6b621
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-11-03 13:08:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,14 +57,14 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Kernel Variant
 NAMA=Wizard
 JENIS=Electro
-VARIAN=NLV
+VARIAN=LV
 
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
