039f58bb18c5539337418be7ef8b9bfdbc1b2176
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-11-11 07:06:54 +0700

    test with clang

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
@@ -196,7 +196,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="queen"
     export KBUILD_BUILD_HOST="android-build"
-    export KBUILD_BUILD_VERSION="3"
+    export KBUILD_BUILD_VERSION="33"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
