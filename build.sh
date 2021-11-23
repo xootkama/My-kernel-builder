115de25c479b6a3d5916abd3ad2d0722f6baed9b
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-11-23 22:15:21 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -196,7 +196,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="queen"
     export KBUILD_BUILD_HOST="18ded16aaef9"
-    export KBUILD_BUILD_VERSION="1"
+    export KBUILD_BUILD_VERSION="2"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
